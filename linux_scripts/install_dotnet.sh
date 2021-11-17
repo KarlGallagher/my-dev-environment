1ffdb6f768e18c7221e3d21266af47c0062d7e2e
Karl Gallagher <Karl.Gallagher@payroc.com>
2021-11-17 16:40:05 +0000

    Install dotnet 6 by default

--- linux_scripts/install_dotnet.sh
+++ linux_scripts/install_dotnet.sh
@@ -1,5 +1,5 @@
 curl -fsSL https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -o packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
-apt-get install -y apt-transport-https &&  apt-get update && apt-get install -y dotnet-sdk-5.0
-apt-get install -y apt-transport-https &&  apt-get update && apt-get install -y aspnetcore-runtime-5.0
+apt-get install -y apt-transport-https &&  apt-get update && apt-get install -y dotnet-sdk-6.0
+apt-get install -y apt-transport-https &&  apt-get update && apt-get install -y aspnetcore-runtime-6.0
